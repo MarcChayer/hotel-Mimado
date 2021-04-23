5b12fb96352153aca39dd545375a2bd713f6f002
MarcChayer <marcci@mac.com>
2021-04-23 21:30:49 +0200

    FEAT contact component mobile and desktop ok

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,33 +10,41 @@ import './contact.scss';
 const Contact = () => (
   <div className="contact">
     <h1 className="titleContact">contact</h1>
-    <div className="formContainer">
-      <p className="formTextContainer">Si vous souhaitez faire une réservation ou avoir plus d’information, contactez-nous :</p>
-      <form action="" className="form">
-        <label className="formLabel">
-          Nom
-        </label>
-        <input type="text" name="Nom" placeholder="Nom" className="formInput" autoComplete="off"/>
-        <label className="formLabel">
-          Entreprise
-        </label>
-          <input type="text" name="Entreprise" placeholder="Entreprise" className="formInput" autoComplete="off"/>
-        <label className="formLabel">
-          Mail
-        </label>
-          <input type="email" name="email" placeholder="Mail" className="formInput" autoComplete="off"/>
-        <label className="formLabel">
-          Message
-        </label>
+    <section className="containerContact">
+      <div className="formContainer">
+        <p className="formTextContainer">Si vous souhaitez faire une réservation ou avoir plus d’information, contactez-nous :</p>
+        <form action="" className="form">
+            <label className="formLabel">
+              Nom
+            </label>
+            <input type="text" name="Nom" placeholder="Nom" className="formInput" autoComplete="off"/>
+            <label className="formLabel">
+              Prénom
+            </label>
+            <input type="text" name="Prénom" placeholder="Prénom" className="formInput" autoComplete="off"/>
+            <label className="formLabel">
+              Entreprise
+            </label>
+            <input type="text" name="Entreprise" placeholder="Entreprise" className="formInput" autoComplete="off"/>
+            <label className="formLabel">
+              Mail
+            </label>
+            <input type="email" name="email" placeholder="Mail" className="formInput" autoComplete="off"/>
+          <label className="formLabel">
+            Message
+          </label   >
           <textarea type="text" name="" placeholder="Message" className="formInput" autoComplete="off" />
           <div>
-            <button>Envoyer</button>
+            <button className="formSubmit">Envoyez</button>
           </div>
-      </form>
-    </div>
-    <div className="groupMapCameroun">
-      <img className="mapCameroun" src={map} alt="illustration représentant le cameroun."/>
-    </div>
+        </form>
+      </div>
+      <div className="groupMapCameroun">
+        <div className="mapCamerounGradient"></div>
+        <img className="mapCameroun" src={map} alt="illustration représentant le cameroun."/>
+        <div className="mapCamerounGradientBottom"></div>
+      </div>
+    </section>
   </div>
 );
 
