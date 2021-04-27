3aba52c5ff9b146b0e5083e57f0254dfdd6473cd
MarcChayer <marcci@mac.com>
2021-04-27 14:28:10 +0200

    FEAT margin bottom of first text on container of contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -49,7 +49,6 @@ const Contact = () => {
       toast.error('Tous les champs du formulaire sont obligatoires, mis à part celui de l\'entreprise.', { className:"toast__error" });
     } else if (!isEmail()) {
       toast.error('Votre adresse mail ne semble pas valide, rentrer une adresse valide.', { className:"toast__error" });
-      console.log('email non conforme');
     }
   }
 
