29c0cdcbb79f1e1d8d7b7f132178799749a53f13
MarcChayer <marcci@mac.com>
2021-05-03 21:10:44 +0200

    FIX info alert form of contact page ok

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,10 @@
 // == Import npm
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
-// import { ToastContainer, toast } from 'react-toastify';
 import { Helmet } from 'react-helmet';
 
 // == Import style
 import './contact.scss';
-// import 'react-toastify/dist/ReactToastify.css';
 
 // == Import image
 import map from '../../assets/images/camerounMap.svg';
@@ -14,7 +12,6 @@ import map from '../../assets/images/camerounMap.svg';
 
 // == Composant
 const Contact = () => { 
-  // toast.configure();
 
   const [name, setName] = useState("");
   const [company, setCompany] = useState("");
@@ -39,6 +36,7 @@ const Contact = () => {
     if (name  && message && isEmail()) {
       // style des infos concernant les mentions obligatoire
       requiredAste.style.backgroundColor = 'transparent';
+      requiredAste.style.border = 'none';
       requiredAste.style.padding = '0em';
       requiredAste.style.textAlign = 'left';        
       requiredAste.style.marginBottom = '1.5em';
@@ -46,7 +44,6 @@ const Contact = () => {
       emailjs.sendForm("service_aht1vfo", "template_jao6lyh", event.target, "user_9coErnt98IbJYjURIIR9L")
         .then((res) => {
           succesSubmit.style.display = 'block';
-          // toast.success('Nous accusons bonne réception de votre message, vous aurez un retour rapidement.', { className:"toast__success" });
           setName("");
           setCompany("");
           setEmail("");
@@ -54,17 +51,16 @@ const Contact = () => {
         });
     };
     if (!name || !message) {
-      // toast.error('Tous les champs du formulaire marqués d\'un astérisque * sont obligatoires.', { className:"toast__error" });
       document.getElementById('name').classList.add('required');
       document.getElementById('message').classList.add('required');
       requiredAste.style.color = 'white';
+      requiredAste.style.border = '3px solid #e74c3d';
       requiredAste.style.backgroundColor = '#e74c3d';
-      requiredAste.style.padding = '.5em 0em';
+      requiredAste.style.padding = '.5em .5em';
       requiredAste.style.textAlign = 'center';        
-      requiredAste.style.marginBottom = '.5em';
+      requiredAste.style.marginBottom = '.8em';
     };
     if (!isEmail()) {
-      // toast.error('Votre adresse mail ne semble pas valide, rentrer une adresse valide.', { className:"toast__error" });
       document.getElementById('email').classList.add('required');
       requiredMail.style.display = 'block';
       requiredMail.style.transition = '.8s ease-in-out';
@@ -95,49 +91,49 @@ const Contact = () => {
         <div className="formContainer">
           <p className="formTextContainer">Si vous souhaitez faire une réservation ou avoir plus d’informations, contactez-nous :</p>
           <p className="formTextContainer2" id="requiredAste">(Les champs marqués d'un astérisque (*) sont obligatoires.)</p>
-          <p id="mailError">Votre adresse mail ne semble pas valide, rentrer une adresse valide.</p>
           <p id="succesSubmit">Nous accusons bonne réception de votre message, vous aurez un retour rapidement.</p>
           <form method="post" className="form" onSubmit={handleOnSubmit}>
-              <label className="formLabel">
-                Prénom et nom*
-              </label>
-              <input
-                onChange={(e) => setName(e.target.value)}
-                type="text"
-                name="name"
-                value={name}
-                placeholder="Votre prénom et nom"
-                className="formInput"
-                id="name"
-                maxlength="45"
-                autoComplete="off"
-              />
-              <label className="formLabel">
-                Entreprise
-              </label>
-              <input
-                onChange={(e) => setCompany(e.target.value)}
-                type="text"
-                name="company"
-                value={company}
-                placeholder="Votre entreprise"
-                className="formInput"
-                maxlength="45"
-                autoComplete="off"
-              />
-              <label className="formLabel">
-                Mail*
-              </label>
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                name="email"
-                value={email}
-                placeholder="Votre mail"
-                className="formInput"
-                id="email"
-                maxlength="254"
-              />
+            <label className="formLabel">
+              Prénom et nom*
+            </label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              name="name"
+              value={name}
+              placeholder="Votre prénom et nom"
+              className="formInput"
+              id="name"
+              maxlength="45"
+              autoComplete="off"
+            />
+            <label className="formLabel">
+              Entreprise
+            </label>
+            <input
+              onChange={(e) => setCompany(e.target.value)}
+              type="text"
+              name="company"
+              value={company}
+              placeholder="Votre entreprise"
+              className="formInput"
+              maxlength="45"
+              autoComplete="off"
+            />
+            <label className="formLabel">
+              Mail*
+            </label>
+            <p id="mailError">Veuillez entrer une adresse e-mail valide.</p>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Votre mail"
+              className="formInput"
+              id="email"
+              maxlength="254"
+            />
             <label className="formLabel">
               Message*
             </label>
